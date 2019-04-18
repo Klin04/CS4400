54c9329f288cbf4866efbce769d445a5677fdf22
Klin04 <kaijun_lin@yahoo.com>
2019-04-17 20:57:57 -0400

    registration user

--- GUI.py
+++ GUI.py
@@ -1,56 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 
-
-# class MainWindow(QtWidgets.QWidget):
-
-#     switch_window = QtCore.pyqtSignal(str)
-
-#     def __init__(self):
-#         QtWidgets.QWidget.__init__(self)
-#         self.setWindowTitle('Main Window')
-
-#         layout = QtWidgets.QGridLayout()
-
-#         self.line_edit = QtWidgets.QLineEdit()
-#         layout.addWidget(self.line_edit)
-
-#         self.button = QtWidgets.QPushButton('Switch Window')
-#         self.button.clicked.connect(self.switch)
-#         layout.addWidget(self.button)
-
-#         self.setLayout(layout)
-
-#     def switch(self):
-#         self.switch_window.emit(self.line_edit.text())
-
-
-class RegisterNavigation(QtWidgets.QWidget):
-
-    def __init__(self):
-        QtWidgets.QWidget.__init__(self)
-        self.setWindowTitle('Register Navigation')
-        self.title = QtWidgets.QLabel()
-        self.title.setText("Register Navigation")
-        self.title.setAlignment(QtCore.Qt.AlignCenter)
-        self.userOnly = QtWidgets.QPushButton('User Only', self)
-        self.visitorOnly = QtWidgets.QPushButton('Visitor Only', self)
-        self.employeeOnly = QtWidgets.QPushButton('Employee Only', self)
-        self.employeeVisitor = QtWidgets.QPushButton('Employee-Visitor', self)
-        self.back = QtWidgets.QPushButton('Back', self)
-
-        layout = QtWidgets.QGridLayout()
-
-        layout.addWidget(self.title)
-        layout.addWidget(self.userOnly)
-        layout.addWidget(self.visitorOnly)
-        layout.addWidget(self.employeeOnly)
-        layout.addWidget(self.employeeVisitor)
-        layout.addWidget(self.back)
-
-        self.setLayout(layout)
-
-
 class Login(QtWidgets.QWidget):
 
     switch_window = QtCore.pyqtSignal(str)
@@ -93,6 +43,113 @@ class Login(QtWidgets.QWidget):
         self.cams = RegisterNavigation()
         self.cams.show()
         self.close()
+
+
+class RegisterNavigation(QtWidgets.QWidget):
+
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Register Navigation')
+        self.title = QtWidgets.QLabel()
+        self.title.setText("Register Navigation")
+        self.title.setAlignment(QtCore.Qt.AlignCenter)
+        self.userOnly = QtWidgets.QPushButton('User Only', self)
+        self.userOnly.clicked.connect(self.userOnlyB)
+        self.visitorOnly = QtWidgets.QPushButton('Visitor Only', self)
+        self.employeeOnly = QtWidgets.QPushButton('Employee Only', self)
+        self.employeeVisitor = QtWidgets.QPushButton('Employee-Visitor', self)
+        self.back = QtWidgets.QPushButton('Back', self)
+        self.back.clicked.connect(self.backB)
+
+        layout = QtWidgets.QGridLayout()
+
+        layout.addWidget(self.title)
+        layout.addWidget(self.userOnly)
+        layout.addWidget(self.visitorOnly)
+        layout.addWidget(self.employeeOnly)
+        layout.addWidget(self.employeeVisitor)
+        layout.addWidget(self.back)
+
+        self.setLayout(layout)
+
+    def userOnlyB(self):
+        self.cams = userOnlyRegistration()
+        self.cams.show()
+        self.close()
+
+    def visitorOnlyB(self):
+        self.cams = visitorOnlyRegistration()
+        self.cams.show()
+        self.close()
+
+    def employeeOnlyB(self):
+        self.cams = employeeOnlyRegistration()
+        self.cams.show()
+        self.close()
+
+    def employeeVisitorB(self):
+        self.cams = employeeVisitorRegistration()
+        self.cams.show()
+        self.close()
+    
+    def backB(self):
+        self.cams = Login()
+        self.cams.show()
+        self.close()
+
+
+class userOnlyRegistration(QtWidgets.QWidget):
+
+    switch_window = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('User Only Registration')
+        self.title = QtWidgets.QLabel()
+        self.title.setText("Register User")
+        self.fnameLabel = QtWidgets.QLabel()
+        self.fnameLabel.setText('First Name')
+        self.lnameLabel = QtWidgets.QLabel()
+        self.lnameLabel.setText('Last Name')
+        self.usernameLabel = QtWidgets.QLabel()
+        self.usernameLabel.setText('Username')
+        self.passwordLabel = QtWidgets.QLabel()
+        self.passwordLabel.setText('Password')
+        self.confpasswordLabel = QtWidgets.QLabel()
+        self.confpasswordLabel.setText('Confirm Password')
+        self.textFname = QtWidgets.QLineEdit()
+        self.textLname = QtWidgets.QLineEdit()
+        self.textusername = QtWidgets.QLineEdit()
+        self.textpassword = QtWidgets.QLineEdit()
+        self.textConfirmPass = QtWidgets.QLineEdit()
+        self.emailLabel = QtWidgets.QLabel()
+        self.emailLabel.setText('Email')
+        self.emailEnter = QtWidgets.QLineEdit()
+        self.registerbutton = QtWidgets.QPushButton('Register', self)
+        self.backButton = QtWidgets.QPushButton('Back', self)
+        self.backButton.clicked.connect(self.backB)
+        layout = QtWidgets.QGridLayout()
+
+        layout.addWidget(self.title)
+        layout.addWidget(self.fnameLabel)
+        layout.addWidget(self.textFname)
+        layout.addWidget(self.lnameLabel)
+        layout.addWidget(self.textLname)
+        layout.addWidget(self.usernameLabel)
+        layout.addWidget(self.textusername)
+        layout.addWidget(self.passwordLabel)
+        layout.addWidget(self.textpassword)
+        layout.addWidget(self.confpasswordLabel)
+        layout.addWidget(self.textConfirmPass)
+        layout.addWidget(self.emailLabel)
+        layout.addWidget(self.emailEnter)
+        self.setLayout(layout)
+
+        def backB(self):
+            self.cams = RegisterNavigation()
+            self.cams.show()
+            self.close()
+
 
 class Controller:
 
