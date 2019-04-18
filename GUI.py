f89a9ceeaa8f06d837ebd89b06912b35c07a8718
Klin04 <kaijun_lin@yahoo.com>
2019-04-17 20:10:14 -0400

    update gui

--- GUI.py
+++ GUI.py
@@ -1,5 +1,109 @@
 import sys
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
+
 
 class MainWindow(QtWidgets.QWidget):
-    
+
+    switch_window = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Main Window')
+
+        layout = QtWidgets.QGridLayout()
+
+        self.line_edit = QtWidgets.QLineEdit()
+        layout.addWidget(self.line_edit)
+
+        self.button = QtWidgets.QPushButton('Switch Window')
+        self.button.clicked.connect(self.switch)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def switch(self):
+        self.switch_window.emit(self.line_edit.text())
+
+
+class Register(QtWidgets.QWidget):
+
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Register')
+
+        layout = QtWidgets.QGridLayout()
+
+        self.
+
+        self.button = QtWidgets.QPushButton('Close')
+        self.button.clicked.connect(self.close)
+
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+
+class Login(QtWidgets.QWidget):
+
+    switch_window = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Login Page')
+        self.textName = QtWidgets.QLineEdit(self)
+        self.textPass = QtWidgets.QLineEdit(self)
+        self.buttonLogin = QtWidgets.QPushButton('Login', self)
+        self.buttonRegister = QtWidgets.QPushButton('Register', self)
+        self.buttonLogin.clicked.connect(self.handleLogin)
+        self.buttonRegister.clicked.connect(self.handleRegister)
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.textName)
+        layout.addWidget(self.textPass)
+        layout.addWidget(self.buttonLogin)
+        layout.addWidget(self.buttonRegister)
+
+    def handleLogin(self):
+        if (self.textName.text() == 'foo' and
+            self.textPass.text() == 'bar'):
+            self.accept()
+        else:
+            QtWidgets.QMessageBox.warning(
+                self, 'Error', 'Bad user or password')
+
+    def handleRegister(self):
+        self.cams = Register()
+        self.cams.show()
+        self.close()
+
+class Controller:
+
+    def __init__(self):
+        pass
+
+    def show_login(self):
+        self.login = Login()
+        self.login.switch_window.connect(self.show_main)
+        self.login.show()
+
+    def show_main(self):
+        self.window = MainWindow()
+        self.window.switch_window.connect(self.show_register)
+        self.login.close()
+        self.window.show()
+
+    def show_register(self, text):
+        self.register = Regsiter(text)
+        self.window.close()
+        self.register.show()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    controller = Controller()
+    controller.show_login()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
