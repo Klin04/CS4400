15104054181baadb0a96fed7477defa57c185362
klin04 <kaijun_lin@yahoo.com>
2019-04-18 23:42:54 -0400

    Added register navigation button actions

--- Controller.py
+++ Controller.py
@@ -2,12 +2,20 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Login import Ui_MainWindow as LoginPage
 from RegisterNavigation import Ui_Form as RegisterNavigationPage
+import RegisterUser
+import RegisterEmployee
+import RegisterEmployeeVisitor
+import RegisterVisitorOnly
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.LoginPage = LoginPage()
         self.RegisterNavigationPage = RegisterNavigationPage()
+        self.RegisterUserPage = RegisterUser.Ui_Form()
+        self.RegisterEmployeePage = RegisterEmployee.Ui_Form()
+        self.RegisterEmployeeVisitorPage = RegisterEmployeeVisitor.Ui_Form()
+        self.RegisterVisitorOnlyPage = RegisterVisitorOnly.Ui_Form()
 
     def startRegisterNavigation(self, parent=None):
         self.RegisterNavigationPage.setupUi(self)
@@ -17,6 +25,22 @@ class MainWindow(QtWidgets.QMainWindow):
         self.LoginPage.setupUi(self)
         self.show()
 
+    def startRegisterUser(self):
+        self.RegisterUserPage.setupUi(self)
+        self.show()
+
+    def startRegisterVisitorOnly(self):
+        self.RegisterVisitorOnlyPage.setupUi(self)
+        self.show()
+
+    def startRegisterEmployee(self):
+        self.RegisterEmployeePage.setupUi(self)
+        self.show()
+
+    def startRegisterEmployeeVisitor(self):
+        self.RegisterEmployeeVisitorPage.setupUi(self)
+        self.show()
+
 
 class Controller():
     def __init__(self):
@@ -24,17 +48,44 @@ class Controller():
     
     def showLogin(self):
         self.MainWindow.close()
+        self.MainWindow = MainWindow()
         self.MainWindow.startLoginPage()
-        self.MainWindow.LoginPage.pushButton_2.clicked.connect(self.showRegNav)
-    
-    def showRegNav(self):
+        self.MainWindow.LoginPage.pushButton_2.clicked.connect(self.showRegisterNavigation)
+
+    def showRegisterNavigation(self):
         self.MainWindow.close()
         self.MainWindow = MainWindow()
         self.MainWindow.startRegisterNavigation()
-    
+        self.MainWindow.RegisterNavigationPage.pushButton.clicked.connect(self.showRegisterUser)
+        self.MainWindow.RegisterNavigationPage.pushButton_2.clicked.connect(self.showRegisterVisitorOnly)
+        self.MainWindow.RegisterNavigationPage.pushButton_3.clicked.connect(self.showRegisterEmployee)
+        self.MainWindow.RegisterNavigationPage.pushButton_4.clicked.connect(self.showRegisterEmployeeVisitor)
+        self.MainWindow.RegisterNavigationPage.pushButton_5.clicked.connect(self.showLogin)
+
     def showRegisterUser(self):
         self.MainWindow.close()
         self.MainWindow = MainWindow()
+        self.MainWindow.startRegisterUser()
+        self.MainWindow.RegisterUserPage.pushButton_2.clicked.connect(self.showRegisterNavigation)
+
+    def showRegisterVisitorOnly(self):
+        self.MainWindow.close()
+        self.MainWindow = MainWindow()
+        self.MainWindow.startRegisterVisitorOnly()
+        self.MainWindow.RegisterVisitorOnlyPage.pushButton_2.clicked.connect(self.showRegisterNavigation)
+
+    def showRegisterEmployee(self):
+        self.MainWindow.close()
+        self.MainWindow = MainWindow()
+        self.MainWindow.startRegisterEmployee()
+        self.MainWindow.RegisterEmployeePage.pushButton_2.clicked.connect(self.showRegisterNavigation)
+
+    def showRegisterEmployeeVisitor(self):
+        self.MainWindow.close()
+        self.MainWindow = MainWindow()
+        self.MainWindow.startRegisterEmployeeVisitor()
+        self.MainWindow.RegisterEmployeeVisitorPage.pushButton_2.clicked.connect(self.showRegisterNavigation)
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
