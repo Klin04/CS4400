2e56e3a45f2801d9dcb46e6dbe92e38577766a41
Samuel Huang <ihuang9@gatech.edu>
2019-04-21 01:25:05 -0400

    wrong password

--- DataBaseManager.py
+++ DataBaseManager.py
@@ -4,7 +4,7 @@ import base64
 mydb = pymysql.connect(
     host="localhost",
     user="root",
-    passwd="joseph1",
+    passwd="Lkj!19990424",
     database="project3",
     cursorclass=pymysql.cursors.DictCursor
 )
